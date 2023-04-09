262c93b3581adf8fa914c6df5d7f3d6d067e1268
allanjustine <labya200@gmail.com>
2023-04-09 19:49:09 +0800

    Adding to deliver

--- app/Http/Livewire/Admin/Orders/Index.php
+++ app/Http/Livewire/Admin/Orders/Index.php
@@ -41,7 +41,7 @@ class Index extends Component
 
         alert()->success('Congrats', 'The order is now paid');
 
-        return redirect('/admin/orders');
+        return redirect('/admin/product-sales');
     }
 
     public function render()
